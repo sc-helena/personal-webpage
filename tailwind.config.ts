66ecc5e6e403d790fd830fae1694fd834a67c80c
sc-helena <helena.schneider@outlook.de>
2025-02-06 11:05:12 +0100

    add project setup

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import { skeleton } from '@skeletonlabs/tw-plugin'
+import { myCustomTheme } from './my-custom-theme'
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		skeleton({
+			themes: {
+				custom: [
+					myCustomTheme
+				]
+			},
+		}),
+	],
+} satisfies Config;
