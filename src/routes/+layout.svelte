a185cd00a64ec33da59e00cc1ae6363a7d1281ee
sc-helena <helena.schneider@outlook.de>
2025-02-10 11:51:06 +0100

    add routings to / and /bio

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,29 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+	import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
 
 	let tabSet: number = 0;
 </script>
 
 <div class="h-4/5">
 	<TabGroup class="[&>.tab-list]:flex [&>.tab-list]:flex-row text-tertiary-500 text-xl">
-		<Tab
+		<TabAnchor
+			href="/"
+			selected={$page.url.pathname === '/'}
 			class="basis-1/3"
 			active="border-b-2 border-b-primary-500"
-			bind:group={tabSet}
-			name="home"
-			value={0}>Home</Tab
 		>
-		<Tab
+			<span>Home</span>
+		</TabAnchor>
+		<TabAnchor
+			href="/bio"
+			selected={$page.url.pathname === '/bio'}
 			class="basis-1/3"
 			active="border-b-2 border-b-primary-500"
-			bind:group={tabSet}
-			name="home"
-			value={1}>Bio</Tab
 		>
+			<span>Bio</span>
+		</TabAnchor>
 		<Tab
 			class="basis-1/3"
 			active="border-b-2 border-b-primary-500"
